c1ffc045e94f5fceabea52618114328918a803fd
Gourav Singh Rawat <grvrawat999@gmail.com>
2021-12-03 20:17:05 +0530

    handled post-request

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const validator = require("validator");
+
 const port = process.env.PORT || 8000;
 const app = express();
+
+const Student = require("./models/students");
+require("./db/conn")
 
 // Create new students 
 app.get("/", (req, res) => {
   res.send("Hello World");
-})
+});
+
+app.use(express.json());
+
 app.post("/students", (req, res) => {
-  res.send("/students");
-})
+  console.log(req.body);
+  const user = new Student(req.body);
+  user.save().then(() => {
+    res.status(201).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
 
 app.listen(port, () => {
   console.log(`Connection at ${port}`);
