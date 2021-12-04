91edafada1a5e651b2ea6100804a89a5cba67e50
Gourav Singh Rawat <grvrawat999@gmail.com>
2021-12-04 14:07:44 +0530

    handled get-request
    reading data with get request find by ID and Name(commented out)

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 
+// Creating data
+
 // app.post("/students", (req, res) => {
 //   console.log(req.body);
 //   const user = new Student(req.body);
@@ -34,6 +36,47 @@ app.post("/students", async (req, res) => {
   }
 });
 
+// Reading data
+
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.send(studentData);
+    }
+    res.send(studentsData);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+// Reading data using IDs
+
+app.get("/students/:id", async (req, res) => {
+  try {
+    const _id = await req.params.id;
+    const studentData = await Student.find({ _id });
+    res.send(studentData);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Reading by Student Name
+
+// app.get("/students/:name", async (req, res) => {
+//   try {
+//     const sname = await req.params.name;
+//     const studentData = await Student.find({ name: sname });
+//     res.send(studentData);
+//   } catch (e) {
+//     res.status(500).send(e);
+//   }
+// });
+
+// Listening to port 
 app.listen(port, () => {
   console.log(`Connection at ${port}`);
 });
